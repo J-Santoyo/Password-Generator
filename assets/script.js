d6f70821e9c2c566a9b32ffff1e3a37023eebe19
J-Santoyo <joanasantoyo030@gmail.com>
2021-09-15 23:02:01 -0600

    added variables to generate password

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = passwordGenerator();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,3 +12,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function passwordGenerator( length ) {
+    var length = (length)?(length):(13);
+    var string = "abcdefghijklmnopqrstuvwxyz"; //allows it to grab all 26 letters
+    var numeric = '0123456789'; //allows for selection of numbers
+    var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-='; //grabs special characters
+}
