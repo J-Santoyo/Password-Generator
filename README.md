99bf1322ad895adae769dd2d78ff1a2dd7372601
J-Santoyo <joanasantoyo030@gmail.com>
2021-09-30 20:38:26 -0600

    added prompt for length added confirm to create characters variable based on user input made the while loop use characters for randomization

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Password-Generator
+# JavaScript Password-Generator
 This password generator will allow employees to generate random passwords.
+
+## User Story
+AS AN employee with access to sensitive data
+I WANT to be able to randomly generate a password that meets the acceptance criteria
+SO THAT I am able to easily create a strong password in order to keep sensitive data secure.
+
+## Acceptance Criteria
+
+GIVEN I need a new , secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose the length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+
--- assets/script.js
+++ assets/script.js
@@ -13,25 +13,46 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function passwordGenerator( length ) {
-    var length = (length)?(length):(13);
-    var string = "abcdefghijklmnopqrstuvwxyz"; //allows it to grab all 26 letters
-    var numeric = '0123456789'; //allows for selection of numbers
-    var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-='; //grabs special characters
-    var password = "";
-    var character = "";
-    while( password.length<length ) {
-        letters = Math.ceil(string.length * Math.random()*Math.random());
-        numbers = Math.ceil(numeric.length * Math.random()*Math.random());
-        chars = Math.ceil(punctuation.length * Math.random()*Math.random());
-        hold = string.charAt( letters );
-        hold = (password.length%2==0)?(hold.toUpperCase()):(hold); //forces some selected chars to Upper
-        character += hold;
-        character += numeric.charAt( numbers );
-        character += punctuation.charAt( chars );
-        password = character;
-    }
-    password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-    return password.substr(0,length);
-    }
-    console.log( passwordGenerator() );
+function passwordGenerator(length) {
+  var length = prompt("How long do you want the password to be?")
+  length = (length) ? (length) : (13);
+  var string = "abcdefghijklmnopqrstuvwxyz"; //allows it to grab all 26 letters
+  var numeric = '0123456789'; //allows for selection of numbers
+  var special = '!@#$%^&*()_+~`|}{[]\:;?><,./-='; //grabs special characters
+  var password = "";
+  var wantUpper = confirm("Do you want uppercase characters?")
+  var wantLower = confirm("Do you want lowercase characters?")
+  var wantNumber = confirm("Do you want numeric characters?")
+  var wantSpecial = confirm("Do you want special characters?")
+
+  var characters = "";
+  if (wantUpper) {
+    characters+=string.toUpperCase()
+  }
+  if (wantLower) {
+    characters = characters+string
+  }
+  if (wantNumber) {
+    characters = characters+numeric
+  }
+  if (wantSpecial) {
+    characters = characters+special
+  }
+
+  while (password.length < length) {
+    // letters = Math.ceil(string.length * Math.random() * Math.random());
+    // numbers = Math.ceil(numeric.length * Math.random() * Math.random());
+    // chars = Math.ceil(special.length * Math.random() * Math.random());
+    var index = Math.floor(characters.length*Math.random());
+    var random = characters.charAt(index)
+    password+= random
+    // hold = string.charAt(letters);
+    // hold = (password.length % 2 == 0) ? (hold.toUpperCase()) : (hold); //forces some selected chars to Upper
+    // password += hold;
+    // password += numeric.charAt(numbers);
+    // password += special.charAt(chars);
+    // password = character;
+  }
+  password = password.split('').sort(function () { return 0.5 - Math.random() }).join('');
+  return password.substr(0, length);
+}
